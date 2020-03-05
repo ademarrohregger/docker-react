990822240b3569eb97f3f2af3659c7b22e8dbeb7
ademarrohregger <ademar.rohregger@gmail.com>
2020-03-04 21:59:26 -0300

    added travis deploy config

---
+++ oldies/build/precache-manifest.fd6e75e1e805307fa0db49d37c3e7ee8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b79b6ac8f6d8afa864554022ff2c6ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "549320b7722340b09d14",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "48f7e82d0d8783898cde",
+    "url": "/static/js/2.4024f285.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.4024f285.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "549320b7722340b09d14",
+    "url": "/static/js/main.4235a0dd.chunk.js"
+  },
+  {
+    "revision": "7a008b5c3a6c3fefae51",
+    "url": "/static/js/runtime-main.18445cb3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
